b5c340bb3251a85efcdbe83dc28fd503a7e00f54
Ian lovering <13082457+ianlovering@users.noreply.github.com>
2024-05-02 16:25:11 +0100

    Fix for GCM

--- home/darwin/ian/ian.nix
+++ home/darwin/ian/ian.nix
@@ -5,5 +5,15 @@
   home.homeDirectory = lib.mkForce "/Users/ian";
 
   imports =[ ../../common/common.nix ];
+
+  programs.git = {
+    enable = true;
+    # ...
+    extraConfig = {
+      credential = {
+        helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
+      };
+    };
+  };
 }
 
